721400823f240275cef0e2b67dda867f50a71f76
mostafamahmoudzaki <121122897+mostafamahmoudzaki@users.noreply.github.com>
2022-12-20 21:20:59 +0200

    Add files via upload

---
+++ ConsoleApp_x2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_x2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i, flag, n1, n2;
+            Console.WriteLine("Enter range I.E. N1 AND N2");
+            n1=int.Parse(Console.ReadLine());
+            n2=int.Parse(Console.ReadLine());
+            num = n1;
+            while (num<=n2)
+            {
+                flag = 0;
+                for (i=2;i<num;i++)
+                {
+                    if (num%i==0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag==0)
+                {
+                    Console.WriteLine("number : "+num);
+                    num++;
+                }
+            }
+        }
+    }
+}
